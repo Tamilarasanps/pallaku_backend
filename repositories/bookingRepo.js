c267aeeb31c74319619a604400b57bb73266de06
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-08-25 11:28:21 +0530

    mail

--- repositories/bookingRepo.js
+++ repositories/bookingRepo.js
@@ -2,21 +2,44 @@ const Booking = require("../models/bookingModel");
 const sendMail = require("../utils/mailer");
 
 const sendBookingEmail = async (data) => {
-  const subject = "New Booking Confirmation";
+  const subject = "Your Booking Confirmation – Thank You for Choosing Us!";
+
   const text = `
-    Name: ${data.name}
-    Mobile: ${data.mobile}
-    Email: ${data.email}
-    From: ${data.from}
-    To: ${data.to}
-    Pickup Time: ${data.pickupTime}
-    Vehicle: ${data.vehicle?.type || "-"} (${data.vehicle?.capacity || 0} seats)
-    Total KMs: ${data.totalKms}
-    Base Fare: ₹${data.baseFair}
-    Toll Charge: ₹${data.tollCharge}
-    Driver Allowance: ₹${data.driverAllowance}
-    Permit Charges: ₹${data.permitCharges}
-    Total Fare: ₹${data.totalFare}
+Dear ${data.name},
+
+Thank you for booking with us! Here are your booking details:
+
+Booking Details:
+------------------------
+Name: ${data.name}
+Mobile: ${data.mobile}
+Email: ${data.email}
+From: ${data.from}
+To: ${data.to}
+Pickup Time: ${data.pickupTime}
+
+Vehicle Details:
+------------------------
+Type: ${data.vehicle?.type || "-"}
+Capacity: ${data.vehicle?.capacity || 0} Seats
+
+Fare Details:
+------------------------
+Total KMs: ${data.totalKms}
+Base Fare: ₹${data.baseFair}
+Toll Charges: ₹${data.tollCharge}
+Driver Allowance: ₹${data.driverAllowance}
+Permit Charges: ₹${data.permitCharges}
+------------------------
+Total Fare: ₹${data.totalFare}
+
+Our driver will contact you shortly for further details.  
+Thank you for choosing our services. We look forward to serving you again!
+
+For any queries, please contact us call us at +91-7871237890.
+
+Warm regards,  
+[Shree Pallak Cabs]
   `;
 
   await sendMail(data.email, subject, text);
--- utils/mailer.js
+++ utils/mailer.js
@@ -2,15 +2,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "karthikw2l@gmail.com",         // replace with your email
-    pass: "hxur erhc fvdz uqli",           // use Gmail App Password, not real password
+    user: "pallakcab@gmail.com", // replace with your email
+    pass: "Jsjsndid28hUhu88snsk", // use Gmail App Password, not real password
   },
 });
 
 const sendMail = async (to, subject, text) => {
-
   const mailOptions = {
-    from: 'karthikw2l@gmail.com',
+    from: "pallakcab@gmail.com",
     to: to,
     subject: subject,
     text: text,
