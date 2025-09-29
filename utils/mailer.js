d5e9708b38cd57d0de519296fb4b9cf0c5df27d3
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-09-29 13:01:46 +0530

    bookingRepo

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true, // true for port 465, false for 587
+  secure: false, // true for port 465, false for 587
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
@@ -19,9 +19,8 @@ const sendMail = async (to, subject, text) => {
   };
 
   try {
-
     const info = await transporter.sendMail(mailOptions);
-  
+
     return { success: true, message: "booking successful", info };
   } catch (err) {
     throw new Error(err);
